d673102d81817e012d0ac598cc9cd05c1d691dd4
lihaiming <18210578206@163.com>
2020-08-20 15:43:05 +0800

    项目机构完善

---
+++ kmall-user-service/src/test/java/cn/lhm/kmall/user/KmallUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.lhm.kmall.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KmallUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
